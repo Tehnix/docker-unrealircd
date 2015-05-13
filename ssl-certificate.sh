f305ad58be68b30c74282cd90a3f13d03022ea79
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2015-05-14 01:42:20 +0200

    Remove final SSL certificate.

--- ssl-certificate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-openssl req \
-    -x509 \
-    -newkey rsa:2048 \
-    -keyout server.key.pem \
-    -out server.cert.pem \
-    -days 730 \
-    -nodes \
-    -subj "/C=DK/ST=None/L=Copenhagen/O=codetalk/OU=IT/CN=codetalk.io"
